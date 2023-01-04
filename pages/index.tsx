d12d7360199b777a2852308e4559890455455008
jonathanchang <jonathan.chang@livibank.com>
2023-01-04 16:53:54 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import DemoWidget from '../components/demo-widget'
 
 const Home: NextPage = () => {
   return (
-    <div className='p-4'>
-
-    <DemoWidget></DemoWidget> 
-    </div>
+      <DemoWidget />
   )
 }
 
