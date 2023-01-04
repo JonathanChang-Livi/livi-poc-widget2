b644b5c3c5b88043acf16d6bff77e86c7248466e
jonathanchang <jonathan.chang@livibank.com>
2023-01-04 16:51:41 +0800

    Update demo-widget.tsx

--- components/demo-widget.tsx
+++ components/demo-widget.tsx
@@ -4,15 +4,13 @@ import { Input } from "livi-poc-form"
 const DemoWidget = () => {
     return (
         <Card override>
-            <CardBody>
-                <Stack className="gap-5">
-                    <Input type="text" placeholder="search" id="search" validate={() => true} className='p-2 px-3 bg-primary text-secondary font-bold rounded-lg' override></Input>
-                    <Stack spacing='sm'>
-                        <p className="text-sm text-slate-300 font-semibold">Welcome Back, </p>
-                        <p className="text-2xl text-primary font-extrabold">liviSave User!</p>
-                    </Stack>
+            <Stack className="gap-5">
+                <Input type="text" placeholder="search" id="search" validate={() => true} className='p-2 px-3 bg-primary text-secondary font-bold rounded-lg' override></Input>
+                <Stack spacing='sm'>
+                    <p className="text-sm text-slate-300 font-semibold">Welcome Back, </p>
+                    <p className="text-2xl text-primary font-extrabold">liviSave User!</p>
                 </Stack>
-            </CardBody>
+            </Stack>
         </Card>
     )
 }
